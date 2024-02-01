94710697edc34254f6de7eec6cdfb655071cd9f4
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-02-01 11:52:23 +0600

    structure ready

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = require("./src/routes/api");
-
-const app = new express()
-
-app.use("/",router);
-
-module.exports= app;
--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const app = require("./app")
+const { error } = require("console");
+const fs = require("fs");
+const http  =  require("http");
 
-app.listen(5500,()=>{
-    console.log("Running on port 5500")
+http.createServer((req,res)=>{
+    if(req.url == '/'){
+        // fs.readFile("home.html", "utf-8", (error, data)=>{
+        //     res.write(data);
+        //     res.end();
+        // })
+        res.end("This is Home Page")
+
+    }else if(req.url == '/contact'){
+        // fs.readFile("contact.html", "utf-8", (error, data)=>{
+        //     res.write(data);
+        //     res.end();
+        // })
+        res.end("This is Contact Page")
+    }else if(req.url == '/about'){
+        // fs.readFile("about.html", "utf-8", (error, data)=>{
+        //     res.write(data);
+        //     res.end();
+        // })
+        res.end("This is About Page")
+    }
+
+}).listen(5500,()=>{
+    console.log("Server Running at port 5500....");
 })
