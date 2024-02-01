7955311ba653f7bbd5016790d2450acfbc5bf2b3
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-02-01 18:54:50 +0600

    file write successfully executed

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const { error } = require("console");
 const fs = require("fs");
-const http  =  require("http");
+const http = require("http");
 
-http.createServer((req,res)=>{
-    if(req.url == '/'){
-        // fs.readFile("home.html", "utf-8", (error, data)=>{
-        //     res.write(data);
-        //     res.end();
-        // })
-        res.end("This is Home Page")
-
-    }else if(req.url == '/contact'){
-        // fs.readFile("contact.html", "utf-8", (error, data)=>{
-        //     res.write(data);
-        //     res.end();
-        // })
-        res.end("This is Contact Page")
-    }else if(req.url == '/about'){
-        // fs.readFile("about.html", "utf-8", (error, data)=>{
-        //     res.write(data);
-        //     res.end();
-        // })
-        res.end("This is About Page")
+http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      res.end("This is Home Page");
+    } 
+    else if (req.url == "/contact") {
+      res.end("This is Contact Page");
     }
-
-}).listen(5500,()=>{
+    else if (req.url == "/about") {
+      res.end("This is About Page");
+    }
+    else if (req.url == "/file-write") {
+        fs.writeFile('demo.txt', 'hello world', (error) => {
+            if (error) {
+                console.error('Error writing file:', error);
+                res.end('Failed');
+            }
+            else {
+                console.log('File written successfully');
+                res.end('success');
+            }
+        });
+    }
+  })
+  .listen(5500, () => {
     console.log("Server Running at port 5500....");
-})
+  });
